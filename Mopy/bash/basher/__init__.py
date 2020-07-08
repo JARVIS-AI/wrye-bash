787053f44919f3922dfb9d8171e5113ae01f955d
Infernio <infernio@icloud.com>
2020-07-08 22:26:59 +0200

    SSS Check BSAs after BAIN-installing
    See TODO above it though - this is a temp solution
    
    Mopy/bash/bosh/dds_files.py:
    Tiny dds_files fixup
    
    Benign, since the BSA tab is not enabled yet

--- Mopy/bash/basher/__init__.py
+++ Mopy/bash/basher/__init__.py
@@ -3198,7 +3198,9 @@ class InstallersPanel(BashTab):
         if inis_changed:
             if BashFrame.iniList is not None:
                 BashFrame.iniList.RefreshUI(focus_list=False)
-        bosh.bsaInfos.refresh() # TODO(ut) : add bsas_changed param! (or rather move this inside BAIN)
+        # TODO(ut) : add bsas_changed param! (or rather move this inside BAIN)
+        bosh.bsaInfos.refresh()
+        Link.Frame.warn_corrupted(warn_bsas=True)
 
 #------------------------------------------------------------------------------
 class ScreensList(balt.UIList):
